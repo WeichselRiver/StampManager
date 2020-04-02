2148921c806ca74903797beb6c217f936bb81db1
WeichselRiver <c.weichsel@gmx.de>
2020-04-02 20:42:14 +0200

    Showing list of articles

---
+++ data.py
@@ -0,0 +1,26 @@
+def Articles():
+    articles = [
+        {
+            'id' : 1,
+            'title': 'Article One',
+            'body': 'Body1',
+            'create_date': '04-25-2019'
+        },
+        {
+            'id' : 2,
+            'title': 'Article Two',
+            'body': 'Body2',
+            'create_date': '05-25-2019'
+        },
+        {
+            'id' : 3,
+            'title': 'Article Three',
+            'body': 'Body3',
+            'create_date': '06-25-2019'
+        }
+    ]
+    return articles
+
+
+
+
