73782f98aea51f20078e4f9e0cbd3f0483f95ef5
WeichselRiver <c.weichsel@gmx.de>
2020-04-03 14:15:22 +0200

    Registerform added

--- app.py
+++ app.py
@@ -1,11 +1,15 @@
-from flask import Flask, render_template
+from flask import Flask, render_template,flash, redirect,url_for
+from forms import RegistrationForm, LoginForm
 from data import Articles
 
 app = Flask(__name__)
 
+app.config['SECRET_KEY'] = 'e86448051b5cedf049dec758cc706ae4'
+
 Articles = Articles()
 
 @app.route('/')
+@app.route('/home')
 def home():
     return render_template('home.html')
 
@@ -17,9 +21,22 @@ def about():
 def articles():
     return render_template('articles.html', articles = Articles)
 
-@app.route('/article/<string:id>/')
-def article(id):
-    return render_template('article.html', id=id)
+@app.route('/article/<string:title>/')
+def article(title):
+    return render_template('article.html', title=title)
+
+@app.route("/register", methods=['GET','POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        flash(f'Account created for {form.username.data}!', 'success')
+        return redirect(url_for('home'))
+    return render_template('register.html', title='Register', form=form)
+
+@app.route("/login")
+def login():
+    form = LoginForm()
+    return render_template('login.html', title='Login', form=form)
 
 
 if __name__ == '__main__':
