903d6f5032d99f0242f92fa2fa40478cd5ab8de5
WeichselRiver <c.weichsel@gmx.de>
2020-04-06 10:04:54 +0200

    Update picture  in account added

--- stamp_manager/forms.py
+++ stamp_manager/forms.py
@@ -1,4 +1,6 @@
 from flask_wtf import FlaskForm
+from flask_wtf.file import FileField, FileAllowed
+from flask_login import current_user
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
 from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
 from stamp_manager.models import User
@@ -30,3 +32,24 @@ class LoginForm(FlaskForm):
     remember = BooleanField('Remember me')
     submit = SubmitField('Login')
 
+
+class UpdateAccountForm(FlaskForm):
+    username = StringField('Username', validators=[
+                           DataRequired(), Length(min=2, max=20)])
+    email = StringField('Email', validators=[DataRequired(), Email()])
+
+    picture = FileField('Update Profile picture', validators=[FileAllowed(['jpg','png'])])
+
+    submit = SubmitField('Update')
+
+    def validate_username(self, username):
+        if username.data != current_user.username:
+            user = User.query.filter_by(username=username.data).first()
+            if user:
+                raise ValidationError('Username taken')
+
+    def validate_email(self, email):
+        if email.data != current_user.email:
+            user = User.query.filter_by(email=email.data).first()
+            if user:
+                raise ValidationError('email taken')
--- stamp_manager/routes.py
+++ stamp_manager/routes.py
@@ -1,9 +1,11 @@
 from flask import render_template, flash, redirect, url_for, request
 from stamp_manager import app, db, bcrypt
-from stamp_manager.forms import RegistrationForm, LoginForm
+from stamp_manager.forms import RegistrationForm, LoginForm, UpdateAccountForm
 from stamp_manager.models import User, Post
 from stamp_manager.data import Articles
 from flask_login import login_user, current_user, logout_user, login_required
+import secrets
+import os
 
 
 Articles = Articles()
@@ -70,9 +72,32 @@ def logout():
     logout_user()
     return redirect(url_for('home'))
 
+# funtion to save updated picture and return filename as random hex
+def save_picture(form_picture):
+    random_hex = secrets.token_hex(8)
+    _, f_ext = os.path.splitext(form_picture.filename) # get extension from orig file
+    picture_fn = random_hex + f_ext # build new filename
+    picture_path = os.path.join(app.root_path, 'static/pics', picture_fn) # build new filepath
+    form_picture.save(picture_path)
+    return picture_fn
 
-@app.route("/account")
+
+@app.route("/account", methods=['GET', 'POST'])
 @login_required
 def account():
+    form = UpdateAccountForm()
+    if form.validate_on_submit():
+        if form.picture.data:
+            picture_file = save_picture(form.picture.data) # call function above
+            current_user.image_file = picture_file # update picture
+        current_user.username = form.username.data # update username
+        current_user.email = form.email.data # update email
+        db.session.commit()
+        flash('Account updated', 'success')
+        return redirect(url_for('account'))
+    elif request.method == 'GET':
+        form.username.data = current_user.username
+        form.email.data = current_user.email
+
     image_file = url_for('static', filename="pics/" + current_user.image_file)
-    return render_template('account.html', title='Account', image_file = image_file)
+    return render_template('account.html', title='Account', image_file=image_file, form=form)
