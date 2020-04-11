1fad1e855ad227f8dfc8c67ee87cbbeb10986224
WeichselRiver <c.weichsel@gmx.de>
2020-04-11 17:33:17 +0200

    Pagination CS9

--- stamp_manager/routes.py
+++ stamp_manager/routes.py
@@ -168,8 +168,10 @@ def delete_post(post_id):
 
 
 @app.route('/user/<string:username>')
-def user_post(username):
-    user = User.query.filter_by(username=username).first_or_404()
+def user_posts(username):
     page = request.args.get('page', 1, type=int)
-    posts = Post.query.order_by(Post.date_posted.desc()).paginate(page=page, per_page=3)
-    return render_template('home.html', title="Home", posts=posts)
+    user = User.query.filter_by(username=username).first_or_404()
+    posts = Post.query.filter_by(author=user)\
+        .order_by(Post.date_posted.desc())\
+        .paginate(page=page, per_page=3)
+    return render_template('user_posts.html', user=user, posts=posts)
